967f7509022e6f96ca974c3ccd44904f33b311be
skobi <skobizsolt@gmail.com>
2026-03-07 18:06:08 +0100

    Initial commit

---
+++ src/main/kotlin/com/p10/stats/Application.kt
@@ -0,0 +1,11 @@
+package com.p10.stats
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class P10StatsApplication
+
+fun main(args: Array<String>) {
+	runApplication<P10StatsApplication>(*args)
+}
